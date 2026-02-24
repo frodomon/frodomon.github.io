13d2873c9b483eaa5c987f27f9763d9eacca9806
Alfredo Vásquez <alfredovasquez@MacBook-Air-de-Alfredo.local>
2026-02-24 13:16:32 -0500

    adjusting json ld and analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,12 @@ import { initMenu } from "./modules/menu.js";
 import { initFilters } from "./modules/filters.js";
 import { initMasonry } from "./modules/masonry.js";
 import { initAnimations } from "./modules/animations.js";
-import { initAnalytics } from "./modules/analytics.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHeader();
   initMenu();
   initFilters();
   initAnimations();
-  initAnalytics();
 });
 
 window.addEventListener("load", () => {
